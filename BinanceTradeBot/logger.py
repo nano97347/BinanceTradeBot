934977fd69d3c9e29cc20db0f12f033a94751f4f
Nano <nano97347@gmail.com>
2025-10-09 21:24:35 +0900

    Add logs folder

--- BinanceTradeBot/logger.py
+++ BinanceTradeBot/logger.py
@@ -1,9 +1,12 @@
 import logging
+import os
 class Logger:
     def __init__(self):
+        path = 'logs'
+        filename = 'test.log'
         print('output from Logger class')
         logger = logging.getLogger(__name__)
-        logging.basicConfig(filename='myapp.log', level=logging.INFO)
+        logging.basicConfig(filename=os.path.join(path, filename), level=logging.INFO)
         logger.info('Started')
         logger.info('Doing something')
         logger.info('Finished')
